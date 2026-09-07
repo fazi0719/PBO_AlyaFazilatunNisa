cdd2cf2a717c7099ec4257785bb63d534db28274
Alya Fazilatun Nisa <nisafazilatunalya@mail.com>
2026-09-07 15:20:08 +0700

    perbaikan

--- Praktikum03/MotorDemo.java
+++ Praktikum03/MotorDemo.java
@@ -2,20 +2,39 @@ package Praktikum03;
 
 public class MotorDemo {
     public static void main(String[] args) {
+        // Membuat objek motor1
         Motor motor1 = new Motor();
         motor1.platNomor = "B 0838 XZ";
-        motor1.kecepatan = 50;
+        // Mengatur status mesin motor1 menjadi false (off)
+        int kecepatanBaru = 50;
+        if (!motor1.statusMesin && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor1.kecepatan = kecepatanBaru;
+        }
         motor1.displayInfo();
 
+        // Membuat objek motor2
         Motor motor2 = new Motor();
         motor2.platNomor = "N 9840 AB";
         motor2.statusMesin = true;
-        motor2.kecepatan = 40;
+        kecepatanBaru = 40;
+        if (!motor2.statusMesin && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor2.kecepatan = kecepatanBaru;
+        }
         motor2.displayInfo();
 
+        // Membuat objek motor3
         Motor motor3 = new Motor();
         motor3.platNomor = "D 8343 CV";
-        motor3.kecepatan = 60;
+        kecepatanBaru = 60;
+        if (!motor3.statusMesin && kecepatanBaru > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else {
+            motor3.kecepatan = kecepatanBaru;
+        }
         motor3.displayInfo();
     }
 }
