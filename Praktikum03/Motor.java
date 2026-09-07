6d59fc5115443e938e60b5b7f9344510def63be7
Alya Fazilatun Nisa <nisafazilatunalya@mail.com>
2026-09-07 17:04:37 +0700

    perubahan

--- Praktikum03/Motor.java
+++ Praktikum03/Motor.java
@@ -4,7 +4,31 @@ public class Motor {
     private String platNomor;
     private boolean statusMesin;
     private int kecepatan;
- 
+  
+    // menambahkan  getter dan setter untuk atribut platNomor, statusMesin, dan kecepatan
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor) {
+        this.platNomor = platNomor;
+    }
+
+    public boolean getStatusMesin() {
+        return statusMesin;
+    }
+
+    public void setStatusMesin(boolean statusMesin) {
+        this.statusMesin = statusMesin;
+    }
+
+    public int getKecepatan() {
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan) {
+        this.kecepatan = kecepatan;
+    }
     // method untuk menampilkan informasi motor
     public void displayInfo() { 
         System.out.println("Plat Nomor: " + this.platNomor);
