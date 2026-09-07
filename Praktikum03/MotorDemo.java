fef7d8e338400ed0856a9f86bc9d2bbb6f0aa407
Alya Fazilatun Nisa <nisafazilatunalya@mail.com>
2026-09-07 20:38:08 +0700

    modif 4

--- Praktikum03/MotorDemo.java
+++ Praktikum03/MotorDemo.java
@@ -20,6 +20,13 @@ public class MotorDemo {
         motor3.setPlatNomor("D 8343 CV");
         motor3.setKecepatan(60);
         motor3.displayInfo();
+
+        // Pengujian Modifikasi No. 4: Kecepatan maksimal 100
+        Motor motor4 = new Motor();
+        motor4.setPlatNomor("N 1234 XY");
+        motor4.setStatusMesin(true);
+        motor4.setKecepatan(120); // Diisi nilai melebihi 100
+        motor4.displayInfo();
     }
 }
 
